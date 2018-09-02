28df54880a8ef448e3d2d256c48460fb0c82cace
James Skemp <strivinglife@gmail.com>
2018-09-01 20:53:16 -0500

    Remove unnecessary Scene from MainMenu

--- src/ts/Game.ts
+++ src/ts/Game.ts
@@ -1,5 +1,5 @@
 import * as phaser from "phaser";
-import MainMenuScene from "./Scenes/MainMenuScene";
+import MainMenu from "./Scenes/MainMenu";
 
 const gameConfig: GameConfig = {
 	width: 800,
@@ -15,8 +15,8 @@ export default class Game extends Phaser.Game {
 
 		super(config);
 
-		this.scene.add(MainMenuScene.Name, MainMenuScene);
-		this.scene.start(MainMenuScene.Name);
+		this.scene.add(MainMenu.Name, MainMenu);
+		this.scene.start(MainMenu.Name);
 	}
 };
 
