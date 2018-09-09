1eadb5ece105d1085dd3364847fb6183bc918014
James Skemp <strivinglife@gmail.com>
2018-09-08 21:00:10 -0500

    Add more to monster-related functionality

--- src/ts/Prefabs/Monster.ts
+++ src/ts/Prefabs/Monster.ts
@@ -1,11 +1,20 @@
 import BattleUnit from "./BattleUnit";
+import MonsterGenProperties from "./MonsterGenProperties";
 
 export default class Monster extends BattleUnit {
 
-	public constructor(genProperties) {
+	public constructor(genProperties: MonsterGenProperties, battleLevel: number) {
 		super();
 
 		this.initBattleUnit();
 		this.type = "monster";
+
+		if (!genProperties) {
+			genProperties = new MonsterGenProperties();
+		}
+
+		if (!genProperties.level || genProperties.level < 1) {
+			genProperties.level = battleLevel;
+		}
 	}
 }
---
+++ src/ts/Prefabs/MonsterGenProperties.ts
@@ -0,0 +1,10 @@
+export default class MonsterGenProperties {
+	public level: number;
+	public type;
+	public name: string = null;
+	public customName: string = null;
+
+	public constructor() {
+		// TODO
+	}
+}
