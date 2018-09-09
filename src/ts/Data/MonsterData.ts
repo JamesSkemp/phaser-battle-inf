f509a198d4dc55e131096dc44602bd5ae7b57ccc
James Skemp <strivinglife@gmail.com>
2018-09-09 11:10:26 -0500

    Add monster acts and behaviors

--- src/ts/Data/MonsterData.ts
+++ src/ts/Data/MonsterData.ts
@@ -1,4 +1,127 @@
+import Utilities from "../Prefabs/Utilities";
+
 export default class MonsterData {
+	public static Act = {
+		target_opponent_with_least(stat: string) {
+			return {
+				type: "Select Target",
+				targetType: "Opponent",
+				select: "With Least",
+				stat
+			};
+		}
+
+		, target_ally_with_least(stat: string) {
+			return {
+				type: "Select Target",
+				targetType: "Ally",
+				select: "With Least",
+				stat
+			};
+		}
+
+		, attack_opponent() {
+			return {
+				type: "Action",
+				code: "Attack",
+				target: "Selected Opponent"
+			};
+		}
+
+		, skill_on_ally(skillName: string) {
+			return {
+				type: "Action",
+				code: "Use Skill",
+				target: "Selected Ally",
+				skillName
+			};
+		}
+
+		, skill_on_opponent(skillName: string) {
+			return {
+				type: "Action",
+				code: "Use Skill",
+				target: "Selected Opponent",
+				skillName
+			};
+		}
+
+		, condition_ally_stat(stat: string, comp: string, constant: string, actions) {
+			return {
+				type: "Condition",
+				target: "Selected Ally",
+				actions,
+				functionName: "Stat",
+				stat,
+				comp,
+				constant
+			};
+		}
+	};
+
+	// TODO change to an array instead?
+	public static Behaviors = {
+		basic_attack: {
+			name(level) {
+				return "";
+			}
+			, actions() {
+				return [
+					MonsterData.Act.target_opponent_with_least("hp")
+					, MonsterData.Act.attack_opponent()
+				];
+			}
+			, statMods: {
+			}
+		}
+
+		, basic_soldier: {
+			name(level) {
+				return "Soldier";
+			}
+			, actions() {
+				const skillName = Utilities.selectRandom(this.skills);
+
+				return [
+					MonsterData.Act.target_opponent_with_least("hp")
+					, MonsterData.Act.skill_on_opponent(skillName)
+					, MonsterData.Act.attack_opponent()
+				];
+			}
+			, statMods: {
+				hp: 1.2
+				, sp: 1
+				, attack: 1.2
+			}
+			, skills: ["Slash", "Bash"]
+			, skillLevel: 1
+		}
+
+		, basic_healer: {
+			name(level) {
+				return "Healer";
+			}
+			, actions() {
+				return [
+					MonsterData.Act.target_ally_with_least("hp")
+					, MonsterData.Act.condition_ally_stat(
+						"hp", "<", "50%",
+						[MonsterData.Act.skill_on_ally("Heal")]
+					)
+					, MonsterData.Act.target_opponent_with_least("hp")
+					, MonsterData.Act.attack_opponent()
+				];
+			}
+			, statMods: {
+				hp: 0.8
+				, sp: 2
+				, attack: 0.6
+			}
+			, skills: ["Heal"]
+			, skillLevel: 1
+		}
+	};
+
 	public static LevelBehaviors = [
 		{
 			maxLevel: 5,
