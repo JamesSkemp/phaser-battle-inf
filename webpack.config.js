7d33639fa2cdba8ce71e5e8abdd9a95f44610c3b
James Skemp <strivinglife@gmail.com>
2018-09-11 08:15:18 -0500

    Add support for Webpack instead of gulp to generate dist

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const phaserModule = path.join(__dirname, '/node_modules/phaser/')
+const phaser = path.join(phaserModule, 'src/phaser.js')
+
+module.exports = {
+	entry: {
+		app: ['./src/ts/Game.ts'],
+		vendor: ['phaser']
+	},
+	module: {
+		rules: [
+		{
+			test: /\.tsx?$/,
+			use: 'ts-loader',
+			exclude: /node_modules/
+		}
+		]
+	},
+	resolve: {
+		extensions: [ '.tsx', '.ts', '.js' ]
+	},
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	plugins: [
+		new CopyWebpackPlugin([{
+			from: './node_modules/phaser/dist/phaser.min.js',
+			to: 'lib'
+		}]),
+		new CopyWebpackPlugin([{
+			from: './src/*.html',
+			to: path.resolve(__dirname, 'dist'),
+			flatten: true
+		}]),
+		new CopyWebpackPlugin([{
+			from: './src/*.css',
+			to: path.resolve(__dirname, 'dist'),
+			flatten: true
+		}]),
+		new CopyWebpackPlugin([{
+			from: './src/*.ico',
+			to: path.resolve(__dirname, 'dist'),
+			flatten: true
+		}]),
+		new CopyWebpackPlugin([{
+			from: './src/assets',
+			to: 'assets',
+			ignore: [ '*.md' ]
+		}])
+	  ]
+};
